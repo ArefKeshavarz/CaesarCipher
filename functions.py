57be30ebaea172d996b3157672d170db2c928b33
Aref Keshavarz <arefkeshavarz1220@gmail.com>
2023-10-24 10:49:31 +0330

    1. build functions for decode and encode . 2. manage all exceptions . 3. add all message for user .

--- functions.py
+++ functions.py
@@ -1,10 +1,11 @@
 # function for encode word :
-def encode(word, alphabet, number):
+def encode_decode(word, alphabet, number, activity):
     word = list(word)
     for letter_of_word in word:
-        new_letter_of_word = alphabet[int(alphabet.index(letter_of_word)) + number]
+        if (activity == "encode"):
+            new_letter_of_word = alphabet[int(alphabet.index(letter_of_word)) + number]
+        elif (activity == "decode"):
+            new_letter_of_word = alphabet[int(alphabet.index(letter_of_word)) - number]
         word[int(word.index(letter_of_word))] = new_letter_of_word
     word = "".join(word)
     return word
-# function for decode word :
-
--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 import Letters
 import functions
 
-print("Hello , welcome to the Caesar Cipher :) ")
-word = input("please enter your word : ")
-number = int(input("please enter your number : "))
-word = functions.encode(word, Letters.alphabet, number)
+print(" ----> Hello , welcome to the Caesar Cipher :) <---- ")
+# receive word :
+word = input(" >> please enter your word : ")
+word = word.lower()
+# for manage exceptions :
+while (not word.isalpha()):
+    if (not word.isalpha()):
+        print(" >>> word is wrong , please enter only a word contain alphba : ")
+        word = input(">> please enter your word : ")
+        word = word.lower()
+# receive shift number :
+number_is_wrong = True
+# for manage exceptions :
+while (number_is_wrong):
+    try:
+        number = int(input(" >> please enter shift number : "))
+        number_is_wrong = False
+    except:
+        print(" >>> input is wrong . ")
+        number_is_wrong = True
+# receive activity :
+activity = input(" >> please enter your activity :  'encode' to encrypt , 'decode' to decrypt :")
+# for manage exceptions :
+while (not (activity == "encode" or activity == "decode")):
+    print(" >>> input is wrong .")
+    activity = input(" >> please enter your activity :  'encode' to encrypt , 'decode' to decrypt :")
+word = functions.encode_decode(word, Letters.alphabet, number, activity)
 print(word)
